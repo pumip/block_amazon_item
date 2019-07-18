7f36324ec21cd6aecb1c16815e2dd071fe263ff8
pumip <suteado1111@gmail.com>
2019-07-18 16:31:50 +0900

    replace hardtab to soft

--- block_amazon_item.js
+++ block_amazon_item.js
@@ -1,45 +1,45 @@
 //商品を隠す関数本体
 delete_specific_item	=function(arr, target_id) {
-		href		=document.getElementById('widgetContent');
-		if (href !== null || href !== undefined) {
-				for(i=0; document.getElementById(target_id+i) !== null; i++) {
-						elem				=document.getElementById(target_id+i);
-						haystack		=elem.innerHTML;
-						arr.forEach(function(val) {
-								for($i=0; arr[$i] != undefined; $i++) {
-										if (haystack.indexOf(arr[$i]) !== -1) {
-													console.log(val + 'を非表示'); //デバッグ用
-													elem.style.border												="solid 1px";
-													elem.firstElementChild.style.visibility	="hidden";
-													elem.style.backgroundColor							="gray";
-													break;
-											}
-								}
-						});
-				};
-				document.body.style.backgroundColor	='black';
-				console.log('end');
-				clearInterval(intervalID);
-		}
+    href		=document.getElementById('widgetContent');
+    if (href !== null || href !== undefined) {
+        for(i=0; document.getElementById(target_id+i) !== null; i++) {
+            elem				=document.getElementById(target_id+i);
+            haystack		=elem.innerHTML;
+            arr.forEach(function(val) {
+                for($i=0; arr[$i] != undefined; $i++) {
+                    if (haystack.indexOf(arr[$i]) !== -1) {
+                        console.log(val + 'を非表示'); //デバッグ用
+                        elem.style.border												="solid 1px";
+                        elem.firstElementChild.style.visibility	="hidden";
+                        elem.style.backgroundColor							="gray";
+                        break;
+                    }
+                }
+            });
+        };
+        document.body.style.backgroundColor	='black';
+        console.log('end');
+        clearInterval(intervalID);
+    }
 };
 
 //@rubyfmzk		https://qiita.com/rubyfmzk/items/1902453ca13e4d8662ee
 function getCsv(url) {
-		//CSVファイルを文字列で取得。
-		var txt = new XMLHttpRequest();
-		txt.open('get', url, false);
-		txt.send();
-		//改行ごとに配列化
-	  return txt.responseText.split('\r\n'); //\nではなく\r\nにしないといけない
+    //CSVファイルを文字列で取得。
+    var txt = new XMLHttpRequest();
+    txt.open('get', url, false);
+    txt.send();
+    //改行ごとに配列化
+    return txt.responseText.split('\r\n'); //\nではなく\r\nにしないといけない
 }
 //ボタン押下時に実行する関数
 function hide_button(target_id) {
-		var url	= 'https://dl.dropboxusercontent.com/s/fr7im768zy29cjp/blacklist.csv';
-		var arr = getCsv(url);
-		if (arr === []) {
-				alert('取得したblacklistが空である例外');
-		}
-		intervalID = window.setInterval(delete_specific_item, 2000, arr, target_id); //delete_specific_item内から呼び出すためグローバルスコープ
+    var url	= 'https://dl.dropboxusercontent.com/s/fr7im768zy29cjp/blacklist.csv';
+    var arr = getCsv(url);
+    if (arr === []) {
+    		alert('取得したblacklistが空である例外');
+    }
+    intervalID = window.setInterval(delete_specific_item, 2000, arr, target_id); //delete_specific_item内から呼び出すためグローバルスコープ
 }
 
 // ボタン作成
